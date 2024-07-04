cf0f050c99631a38bed9140946536f204e88ec39
Xavier Maruff <xavier.maruff@stleonards.vic.edu.au>
2024-07-04 17:03:31 +1000

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# zed-fortran
+Fortran language support for the Zed editor.
+
+All SCM files are stolen from [tree-sitter-fortran](https://github.com/stadelmanma/tree-sitter-fortran).
---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use zed_extension_api as zed;
+
+struct FortranExtension {}
+
+impl zed::Extension for FortranExtension {
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn language_server_command(
+        &mut self,
+        _language_server_id: &zed::LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> zed::Result<zed::Command> {
+        let path = worktree
+            .which("fortls")
+            .ok_or_else(|| "Fortls not installed.".to_string())?;
+
+        Ok(zed::Command {
+            command: path,
+            args: vec![],
+            env: Default::default(),
+        })
+    }
+}
+
+zed::register_extension!(FortranExtension);
